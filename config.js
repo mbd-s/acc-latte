3e998b7282a992b4228f03e4ee5316a8d9b70a2c
mbd-s <michaelbsilverberg@gmail.com>
2017-03-16 18:32:28 +0100

    'fixes typo, reverts one production var to dev'

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var config = {};
 config.mongo = {}
 
-var env = process.env.NODE_ENV || 'production';
+var env = process.env.NODE_ENV || 'development';
 
 if (env === 'production'){
-  config.mongo.url = process.env.MONGODOB_URI;
+  config.mongo.url = process.env.MONGODB_URI;
 }
 else {
   config.mongo.url = 'mongodb://localhost:27017/test'
 }
 
 module.exports = config;
-console.log(process.env.MONGODOB_URI);
+console.log(process.env.MONGODB_URI);
