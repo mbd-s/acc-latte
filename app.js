d7c8e0ff363759ac55a0cf72e23231e063bc2d2f
Isaabomohana <mrissaam@outlook.com>
2017-03-21 20:53:08 +0100

    sets up facebook login with passport

--- app.js
+++ app.js
@@ -1,34 +1,52 @@
-var express = require('express')
-var app = express()
-require('dotenv').load()
+var express = require('express');
+var app = express();
+require('dotenv').load();
 
 var config = require('./config');
+
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 var url = config.mongo.url;
 var mongoose = require('mongoose');
 mongoose.connect(url);
 
 var passport = require('passport');
-var session = require('express-session');
-app.use(session({
-    secret: 'mySecretKey',
-    resave: true,
-    saveUninitialized: false
-  }));
-app.use(passport.initialize());
-app.use(passport.session());
+
+passport.use(new FacebookStrategy({
+    clientID: config.facebook.id,
+    clientSecret: config.facebook.appSecret,
+    callbackURL: config.facebook.callBackUrl + '/login/facebook/callback'
+  },
+  function(accessToken, refreshToken, profile, cb) {
+  }
+));
 
 app.get('/', function (req, res) {
-  res.send('Hello, World!')
+  res.send('Hello, World!');
 })
 
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/callback',
+  function(req, res) {
+    res.redirect('/');
+  });
+
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Connected to server')
+  console.log('Connected to server');
 });
 
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-
 
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
--- config.js
+++ config.js
@@ -1,13 +1,18 @@
 var config = {};
 config.mongo = {}
+config.facebook = {}
 
 var env = process.env.NODE_ENV || 'development';
+config.facebook.id = process.env.FACEBOOK_ID;
+config.facebook.appSecret = process.env.FACEBOOK_APP_SECRET;
 
 if (env === 'production'){
   config.mongo.url = process.env.MONGODB_URI;
+  config.facebook.callBackUrl = 'https://acc-latte.herokuapp.com';
 }
 else {
-  config.mongo.url = 'mongodb://localhost:27017/test'
+  config.mongo.url = 'mongodb://localhost:27017/test';
+  config.facebook.callBackUrl = 'http://localhost:3000';
 }
 
 module.exports = config;
