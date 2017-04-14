4205468db70c9a389ad8f2d9ea041686e020c409
Isaabomohana <mrissaam@outlook.com>
2017-04-14 11:25:40 +0200

    add pages & install layouts & extract user FB login token

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var renderLayouts = require('layouts');
 
 require('dotenv').config();
 
@@ -23,6 +24,28 @@ passport.use(new FacebookStrategy({
     callbackURL: config.facebook.callBackUrl + '/login/facebook/callback'
   },
   function(accessToken, refreshToken, profile, cb) {
+    if (user) {
+        res.sendFile('dashboard', { root: views_path } );
+    } else if (err) {
+          return done(err);
+    } else {
+        // if there is no user found with that facebook id, create them
+        var newUser = new User();
+
+        // set all of the facebook information in our user model
+        newUser.facebook.id    = profile.id; // set the users facebook id
+        newUser.facebook.token = token; // we will save the token that facebook provides to the user
+        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+        console.log(profile.id);
+        // save our user to the database
+        newUser.save(function(err) {
+            if (err)
+                throw err;
+            // if successful, return the new user
+            return done(null, newUser);
+        });
+      }
   }
 ));
 
@@ -42,10 +65,13 @@ app.get('/login/facebook/callback',
       //save user if user does not already exists
       //if user already exists update login token
       //else create new user
-      res.sendFile('dashboard.html', { root: views_path });
-    //else
+      //else
       // res.sendFile('index.html'); with error message
-  });
+
+        // if the user is found, then log them in
+
+      });
+
 
 var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Connected to server');
