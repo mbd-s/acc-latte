8a5318740c731269d7acf9dc29e88be38c22d6d4
Isaabomohana <mrissaam@outlook.com>
2017-04-14 13:09:01 +0200

    adjust inDuration

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,6 @@ $(".button-collapse").sideNav();
 $(".animsition").animsition({
   inClass: 'fade-in-left',
   outClass: 'fade-out-right',
-  inDuration: 1000,
+  inDuration: 800,
   outDuration: 800
 });
