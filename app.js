a7ad28df2c0f3be8911ffadbb9ea4606dab48811
Isaabomohana <mrissaam@outlook.com>
2017-04-18 20:18:45 +0200

    inline User schema

--- app.js
+++ app.js
@@ -119,4 +119,18 @@ MongoClient.connect(url, function(err, db) {
   db.close();
 });
 
+var mongoose = require('mongoose');
+var userSchema = mongoose.Schema({
+  facebook: {
+    id: String,
+    name: String,
+    token: String,
+    email: String,
+  }
+});
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User
+
 module.exports = server;
