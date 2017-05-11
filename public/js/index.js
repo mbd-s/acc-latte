fb015e564d98ade3968562b631c1f57fbd280dc7
Isaabomohana <mrissaam@outlook.com>
2017-05-11 15:30:55 +0200

    add dynamincs and cofeeShopInfo object too cofeeShop

--- public/js/index.js
+++ public/js/index.js
@@ -5,15 +5,47 @@ $(document).ready(function(){
       inClass: 'fade-in-down',
       inDuration: 800
     });
+    let cofeeShopInfo = [];
 
     $('#cs-btn').click(function(){
       $('#cofeeShop').addClass('hidin');
       $("#cofeeShop-wel").removeClass("hidin");
     });
 
+
     $('#goAhead').click(function() {
       $('#cofeeShop-wel').addClass('hidin');
-      $("#cofeeShop-info").removeClass("hidin");
+      $("#cofeeShop-name").removeClass("hidin");
     })
+
+    $('#goName').click(function() {
+      const cofeeShopName = $('#cofeeshop-name').val();
+      console.log(cofeeShopName);
+      cofeeShopInfo.push(cofeeShopName);
+      console.log(cofeeShopInfo);
+      $("#cofeeShop-name").addClass("hidin");
+      $("#cofeeShop-rent").removeClass("hidin");
+    })
+
+    $('#goRent').click(function() {
+      let cofeeShopRent ;
+
+      $('#rent-buy option').each(function() {
+        if($('[value="2"]').is(':selected')){
+          cofeeShopRent = "renting";
+        } else if ($('[value="1"]').is(':selected')) {
+          cofeeShopRent = "buying";
+        }
+        console.log(cofeeShopRent);
+        cofeeShopInfo.push(cofeeShopRent);
+        console.log(cofeeShopInfo);
+        $("#cofeeShop-rent").addClass("hidin");
+      })
+    })
+
+
+
+
+
 
 });
