d1e9a3dc1023927a1904a8b2acb3286c3b4e2e9c
Isaabomohana <mrissaam@outlook.com>
2017-05-15 16:39:14 +0200

    finish Assets's slide

--- public/js/index.js
+++ public/js/index.js
@@ -47,36 +47,71 @@ $(document).ready(function(){
     })
 
     $('.expresso').hover(function(){
-      Materialize.toast('I can make atleast 3000 cup :)', 4000, 'expresso');
+      Materialize.toast('Expresso: I can make atleast 3000 cup :)', 3000, 'cyan rounded', 'expresso');
     });
-    $('.nescafe').hover(function(){
-      Materialize.toast('I can make atleast 3500 cup :)', 4000, 'nescafe');
-    });
-    $('.dulungo').hover(function(){
-      Materialize.toast('I can make atleast 4000 cup :)', 4000, 'dulungo');
-    });
-
-    $('.expresso').dblclick(function(){
-      $('.chousen-assts').append('<p>Expresso 300€</p>');
+    $('.expresso').click(function(){
+      $('.choosen-assts').append('<p>Expresso 300€</p>');
       $('.expresso').addClass('hidin');
       asstsTotal = asstsTotal + 300;
-      console.log(asstsTotal);
     });
-    $('.nescafe').dblclick(function(){
-      $('.chousen-assts').append('<p>Nescafe 350€</p>');
+
+    $('.nescafe').hover(function(){
+      Materialize.toast('Nescafe: I can make atleast 3500 cup :)', 3000, 'cyan rounded', 'nescafe');
+    });
+    $('.nescafe').click(function(){
+      $('.choosen-assts').append('<p>Nescafe 350€</p>');
       $('.nescafe').addClass('hidin');
       asstsTotal = asstsTotal + 350;
-      console.log(asstsTotal);
     });
-    $('.dulungo').dblclick(function(){
-      $('.chousen-assts').append('<p>Dulungo 400€</p>');
+
+    $('.dulungo').hover(function(){
+      Materialize.toast('Dulungo: I can make atleast 4000 cup :)', 3000, 'cyan rounded', 'dulungo');
+    });
+    $('.dulungo').click(function(){
+      $('.choosen-assts').append('<p>Dulungo 400€</p>');
       $('.dulungo').addClass('hidin');
       asstsTotal = asstsTotal + 400;
-      console.log(asstsTotal);
     });
 
+    $('.renault').click(function(){
+      $('.choosen-assts').append('<p>Renault 5000€</p>');
+      $('.renault').addClass('hidin');
+      asstsTotal = asstsTotal + 5000;
+    });
+    $('.nissan').click(function(){
+      $('.choosen-assts').append('<p>Nissan 5500€</p>');
+      $('.nissan').addClass('hidin');
+      asstsTotal = asstsTotal + 5500;
+    });
+    $('.mercedes').click(function(){
+      $('.choosen-assts').append('<p>Mercedes Benz 7000€</p>');
+      $('.mercedes').addClass('hidin');
+      asstsTotal = asstsTotal + 7000;
+    });
 
+    $('.takeaway').click(function(){
+      $('.choosen-assts').append('<p>Take away 3000€</p>');
+      $('.takeaway').addClass('hidin');
+      asstsTotal = asstsTotal + 3000;
+    });
+    $('.highchares').click(function(){
+      $('.choosen-assts').append('<p>Hi Chares 4500€</p>');
+      $('.highchares').addClass('hidin');
+      asstsTotal = asstsTotal + 4500;
+    });
+    $('.lether').click(function(){
+      $('.choosen-assts').append('<p>Lether 8000€</p>');
+      $('.lether').addClass('hidin');
+      asstsTotal = asstsTotal + 7000;
+    });
 
+    $('#goAssets').click(function() {
+      if (asstsTotal === 0) {
+        Materialize.toast('You need to pick something dear)', 3000, 'cyan rounded');
+      } else {
+        console.log(asstsTotal);
+      }
+    });
 
 
 
